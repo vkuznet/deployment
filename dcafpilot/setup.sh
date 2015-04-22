aa5bd1e9879cc74d13d0503e6a5db971bf7b38cd
Valentin Kuznetsov <vkuznet@gmail.com>
2015-04-22 08:44:40 -0400

    Change locations of data/predictions; rename monitoring script

--- dcafpilot/setup.sh
+++ dcafpilot/setup.sh
@@ -4,5 +4,5 @@ source $rdir/etc/profile.d/init.sh
 export DCAFPILOT_ROOT
 export X509_USER_CERT={ROOT}/state/dcafpilot/proxy/proxy.cert
 export X509_USER_KEY={ROOT}/state/dcafpilot/proxy/proxy.cert
-export DCAFPILOT_PREDICTIONS={ROOT}/state/dcafpilot/analytics/data/predictions
+export DCAFPILOT_PREDICTIONS={ROOT}/state/dcafpilot/analytics/predictions
 export DCAFPILOT_CONFIG=$rdir/etc/dcaf.cfg
